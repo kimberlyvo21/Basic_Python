4b2e0df6b080d38ecd119f483e1266ec5ddfdf5a
Kimberly Vo <kimberlyvo21@gmail.com>
2019-02-12 17:44:57 -0500

    Add files via upload

---
+++ c2f.py
@@ -0,0 +1,7 @@
+# Kimberly Vo
+# kv3nw
+tempC= input("What is the temperature in Celsius? ")
+tempC=int(tempC)
+# looked up formula from https://www.rapidtables.com/convert/temperature/how-celsius-to-fahrenheit.html
+tempF= round((tempC * 1.8)+32,1)
+print("It is",tempF,"degrees Fahrenheit")
